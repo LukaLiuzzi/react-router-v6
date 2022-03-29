ffa3eb81b69578ee06bb4bede6226140ec80a393
Luka Liuzzi <lukaliuzzidev@gmail.com>
2022-03-28 21:46:54 -0300

    search params and redirect

--- src/routes/Blog.jsx
+++ src/routes/Blog.jsx
@@ -1,11 +1,21 @@
 import { useFetch } from '../hooks/useFetch'
 import Spinner from '../components/Spinner'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 
 const Blog = () => {
 
+  let [searchParams, setSearchParams] = useSearchParams()
+
   const {data, error, loading} = useFetch('https://jsonplaceholder.typicode.com/posts')
 
+  const handleChange = (e) => {
+    let search = e.target.value;
+    if (search) {
+      setSearchParams({ search })
+    } else {
+      setSearchParams({})
+    }
+  }
 
   if (loading) {
     return <Spinner />
@@ -18,8 +28,17 @@ const Blog = () => {
   return (
     <div>
     <h1 className='text-center mt-2 text-uppercase fw-bold'>Blog</h1>
+
+    <input type="text" className='form-control mb-2' value={searchParams.get('search') || ''} name='search' onChange={handleChange} />
+    
     {
-      data.map(post => (
+
+      data.filter(post => {
+        if (searchParams.get('search')) {
+          return post.title.toLowerCase().includes(searchParams.get('search').toLowerCase())
+        } else {
+          return post
+        }}).map(post => (
         <div className='card mb-3' key={post.id}>
           <Link to={`/blog/${post.id}`}>
           <div className='card-body bg-dark text-white'>
